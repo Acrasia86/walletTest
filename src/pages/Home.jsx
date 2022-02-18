9b580a4d1f4d916735d81b87f613d76372549bdd
Jacob Påhlsson <Jacob.pahlsson@hotmail.com>
2022-02-18 01:11:21 +0100

    removed h1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div>
-        <h1 style={{marginBottom: '50px', marginTop: '30px'}}>E wallet</h1>
+        {/* <h1 style={{marginBottom: '50px', marginTop: '30px'}}>E wallet</h1> */}
         <CardList />
         <Link to='/addCard'>
         <button className='addCardBtn'>Add card</button>
